80ceef4459b48d17343edc8fa943671850cfd983
mcc808 <112995753+mcc808@users.noreply.github.com>
2022-11-01 19:59:20 -0700

    server work with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navBar';
+import SideBar from './components/sideBar';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar></NavBar>
+      <SideBar></SideBar>
+      <Footer></Footer>
+      <h1>Hello React!!!</h1>
     </div>
   );
 }
---
+++ src/components/sideBar.jsx
@@ -0,0 +1,11 @@
+import './sideBar.css'
+
+function SideBar(){
+    return(
+        <div className='sidebar'>
+            <h1>View sidebar here</h1>
+        </div>
+    ) 
+}
+
+export default SideBar;
